bc7bdc4dea759c5d3fd5bb838f603ea5dcf8ac68
unknown <mseizduque@gmail.com>
2023-05-01 13:49:13 +0200

    Proyecto_Pokemon

--- js/productos.js
+++ js/productos.js
@@ -6,23 +6,51 @@ menuHamburguesa.classList.toggle('menu-hidden');
 });
 
 const pokemonContainer = document.querySelector(".pokemon-container");
+const spinner = document.querySelector("#spinner");
+
+let offset = 1;
+let limit = 11;
+
+previous.addEventListener("click", () => {
+    if (offset !=1) {
+        offset -= 10;
+        removeChildNodes(pokemonContainer);
+        fetchPokemons(offset, limit);
+    }
+   })
+
+next.addEventListener("click", () => {
+    offset += 10;
+    removeChildNodes(pokemonContainer);
+    fetchPokemons(offset, limit);
+})
 
 function fetchPokemon (id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     .then((res) => res.json())
     .then(data => {
         createPokemon(data);
+        spinner.style.display = "none";
 });
 }
 
-function fetch200Pokemons(number) {
-    for (let i = 1; i <= number; i++) {
+function fetchPokemons(offset, limit) {
+    spinner.style.display = "block";
+    for (let i = offset; i <= offset + limit; i++) {
         fetchPokemon(i);
     }
 }
-fetch200Pokemons(200);
+
 
 function createPokemon(pokemon){
+    const flipCard = document.createElement("div");
+    flipCard.classList.add("flip-card");
+
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card-container");
+
+    flipCard.appendChild(cardContainer);
+
     const card = document.createElement('div'); 
     card.classList.add('pokemon-block');
 
@@ -45,8 +73,55 @@ function createPokemon(pokemon){
     card.appendChild(number);
     card.appendChild(name);
 
-    pokemonContainer.appendChild(card);
+    const cardBack = document.createElement("div");
+    cardBack.classList.add("pokemon-block-back");
 
+    cardBack.appendChild(progressBars(pokemon.stats));
 
+    cardContainer.appendChild(card);
+    cardContainer.appendChild(cardBack);
+    pokemonContainer.appendChild(flipCard);
+}
+    function progressBars(stats){
+    const statsContainer = document.createElement("div");
+    statsContainer.classList.add("stats-container");
+
+    for (let i = 0; i < 3; i++) {
+        const stat  = stats[i];
+
+        const statPercent = stat.base_state / 2 + "%";
+        const statContainer = document.createElement("div");
+        statContainer.classList.add("stat-container");
+
+        const statName = document.createElement("div");
+        statName.textContent = stat.stat.name;
+
+        const progress = document.createElement("div");
+        progress.classList.add("progress");
+
+        const progressBar = document.createElement("div");
+        progressBar.classList.add("progress-bar");
+        progressBar.setAttribute("aria-valuenow", stat.base_stat);
+        progressBar.setAttribute("aria-valuemin", 0);
+        progressBar.setAttribute("aria-valuemax", 200);
+        progressBar.style.width = statPercent;
+
+        progressBar.textContent = stat.base_stat;
+
+        progress.appendChild(progressBar);
+        statContainer.appendChild(statName);
+        statContainer.appendChild(progress);
+        statsContainer.appendChild(statContainer);
+    }
+
+    return statsContainer; 
 }
 
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+   }
+}
+
+
+fetchPokemons(offset, limit);
